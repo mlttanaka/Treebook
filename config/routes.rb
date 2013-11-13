8ba4793db65578ba88a25a99bb0cfe32643fbc80
Meredith Taylor-Tanaka <mlttanaka@mochipuff.net>
2013-11-13 15:49:45 -0800

    Create urls for the routes we want to use.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Treebook::Application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get "register", :to => "devise/registrations#new", as: :register
+    get "sign_in", :to => "devise/sessions#new", as: :sign_in
+    delete "sign_out", :to => "devise/sessions#destroy", as: :sign_out
+  end
+
   resources :statuses
+  get 'feed', to: 'statuses#index', as: :feed
 
   root "statuses#index"
 
