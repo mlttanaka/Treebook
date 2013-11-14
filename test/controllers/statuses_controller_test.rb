81dd3294270c87a61122284337d0eed13f68156b
Meredith Taylor-Tanaka <mlttanaka@mochipuff.net>
2013-11-13 22:19:36 -0800

    Create tests for status controller and status.

--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -21,10 +21,16 @@ class StatusesControllerTest < ActionController::TestCase
     sign_in users(:tester_one)
     get :new
     assert_response :success
-
   end
 
-  test "should create status" do
+  test "should be logged in to post a status" do
+    post :create, status: { content: "Hello"}
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should create status when signed in" do
+    sign_in users(:tester_one)
     assert_difference('Status.count') do
       post :create, status: { content: @status.content }
     end
@@ -32,22 +38,32 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to status_path(assigns(:status))
   end
 
-  test "should show status" do
+  test "should show status when signed in" do
+    sign_in users(:tester_one)
     get :show, id: @status
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should get edit when signed in" do
+    sign_in users(:tester_one)
     get :edit, id: @status
     assert_response :success
   end
 
-  test "should update status" do
+  test "should redirect status update when not signed in" do
+    patch :update, id: @status, status: { content: @status.content }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should update status when signed in" do
+    sign_in users(:tester_one)
     patch :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
 
-  test "should destroy status" do
+  test "should destroy status when signed in" do
+    sign_in users(:tester_one)
     assert_difference('Status.count', -1) do
       delete :destroy, id: @status
     end
