cd13ce495f1be80b443e6a47337da2df06b93dfc
Meredith Taylor-Tanaka <mlttanaka@mochipuff.net>
2013-11-14 23:10:14 -0800

    Fix routing bug so that the link to the user's full names routes to the edit registraion page.  Remove all the custom routes that the tutorial recommended; keep it simple.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,6 @@
 class ProfilesController < ApplicationController
   def show
-    @user = User.find_by_profile_name(params[:id])
+    @user = User.find(params[:id])
     if @user
       @statuses = @user.statuses
       render :show
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,9 @@ Treebook::Application.routes.draw do
   get "profiles/show"
   devise_for :users
 
-  devise_scope :user do
-    get "register", :to => "devise/registrations#new", as: :register
-    get "sign_in", :to => "devise/sessions#new", as: :sign_in
-    delete "sign_out", :to => "devise/sessions#destroy", as: :sign_out
-  end
-
   resources :statuses
-  get 'feed', to: 'statuses#index', as: :feed
 
   root "statuses#index"
 
-  get ':id', to: 'profiles#show'
+  get "profiles/:id", to: "profiles#show", as: "profiles"
 end
